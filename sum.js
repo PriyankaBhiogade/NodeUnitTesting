8857680192176e4b6b0d52fa07f483bed3f88d87
PriyankaBhiogade <bhiogadepriyanka93@gmail.com>
2019-12-28 11:26:06 +0530

    sample test code

---
+++ sum.js
@@ -0,0 +1,11 @@
+function sum(a,b){
+if ( a =="" || b == "")
+	return 0;
+if(typeof(a) === 'undefined' || typeof(b) === 'undefine' )
+	return 0;
+if ( isNaN(a) || isNaN(b))
+	return 0;
+let c = a + b;
+	return c;
+}
+module.exports = sum;
---
+++ sumController.js
@@ -0,0 +1,7 @@
+let sum = require('./sum');
+let err1 = sum( );
+let err2 = sum(2);
+let err3 = sum('abc',2);
+let err4 = sum(" ",2);
+let good = sum(2,3);
+console.log("Res: ",good);
